95eed44ced388582fef2a024757ad7f0c3453369
apoldmaa <apoldmaa@hkhk.edu.ee>
2025-11-06 10:22:55 +0200

    Add files via upload

---
+++ lauad ja toolid.py
@@ -0,0 +1,5 @@
+opilased = int(input("Sisesta Õpilaste arv: "))
+lauad = opilased // 2
+tyhjad_kohad = opilased % 2
+print (f"Laudu on vaja: {lauad}")
+print (f"Õpilasi ilma lauapartnerita = {tyhjad_kohad}")
